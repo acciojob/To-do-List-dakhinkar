5a7a533f228a94470a30712f685f6175027e32ca
Pranit Dakhinkar <97027846+dakhinkar@users.noreply.github.com>
2022-12-06 23:10:53 +0530

    Update script.js
    add event handler

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
 
+const inputTodo = document.getElementById('newTodoInput');
+const todoList = document.getElementById('todoList');
+const addHandler = () => {
+    // console.log("Adda todo");
+    
+    // console.log();
+    if (inputTodo.value.trim() === "") {
+        return;
+    }
+    const node = document.createElement('li');
+    const textNode = document.createTextNode(inputTodo.value);
+    node.appendChild(textNode);
+    todoList.appendChild(node);
+    inputTodo.innerText = "";
+    inputTodo.innerHTML = "";
+    inputTodo.value = "";
+
+}
